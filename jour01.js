a9ffc1badfda1e8ec689f016a9fdaf16e484d9f6
ophélie <ophelieleleu@yahoo.fr>
2021-04-13 15:53:18 +0200

    exo 5

--- jour01.js
+++ jour01.js
@@ -1,10 +1,49 @@
+//01 Hello Word
+
+console.log ("Hello Word !") 
+
+//02 String
+
 var test;
 
+test = "My name is Ophélie";
+
+console.log (test) 
+
+//03 Concatennation
+var name;
+
+name = "Ophélie";
+
+console.log ("Nice to meet you " + name)
+
+
+//04 String Length
+
+var test;
 
 test = "I'm very long !";
 
-
-
-
 console.log (test.length)
+
+
+
+//05 Replace
+
+var food = "croissant in meh";
+
+newFood = food.replace("meh", "sogood");
+
+console.log (newFood);
  
+
+//06 Up and Down
+
+var basic = "This is Cool"
+
+var basicUpperCase = basic.toUpperCase();
+var basicLowerCase = basic.toLowerCase();
+
+console.log (basicUpperCase);
+console.log (basicLowerCase);
+
