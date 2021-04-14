dd620cf5f13eca420302197c3059722328687348
ophélie <ophelieleleu@yahoo.fr>
2021-04-14 13:34:53 +0200

    exo 02

--- jour02.js
+++ jour02.js
@@ -2,8 +2,19 @@
 
 console.log("exo01");
 
-var integer = 102
-var float = 13.9
+var integer = 102;
+var float = 13.9;
 
 console.log(integer);
 console.log(float);
+
+
+// 02 Convert
+
+console.log("exo02");
+
+var basic = 34;
+var stringified = "34";
+
+console.log(stringified);
+
