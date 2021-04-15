c6a567bea66dedff43724e3cbfb411892d1cf9d9
ophélie <ophelieleleu@yahoo.fr>
2021-04-15 13:21:28 +0200

    Exo 02

--- jour03.js
+++ jour03.js
@@ -1,9 +1,23 @@
 // 01 Arrays
 
 console.log("Exo 01");
+
 var fruits = ["mango", "lemon", "blueberry"];
 
 console.log(fruits);
 console.table(fruits);
 
 console.log();
+
+
+// 02 Access
+
+console.log("Exo 02 ");
+
+var ingredients = ["eggs", "milk", "butter"];
+
+console.log(ingredients[1]);
+console.log(ingredients.indexOf("butter"));
+
+console.log();
+
