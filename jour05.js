6da54359daf2ad588678e56965ed2e5f35742bc5
ophélie <ophelieleleu@yahoo.fr>
2021-04-19 17:12:44 +0200

    Exo 01 whileloop

--- jour05.js
+++ jour05.js
@@ -65,5 +65,3 @@ console.log(entier);
 console.log();
 
 
-
-
---
+++ whileloop.js
@@ -0,0 +1,12 @@
+var finishPosition = 200;
+var startPosition = 50;
+
+
+
+
+while (startPosition <= finishPosition) {
+  console.log(startPosition++);
+  if (startPosition++ % 0) {
+    }
+}
+
