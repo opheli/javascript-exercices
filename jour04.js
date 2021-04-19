40252395d796b02f555886f66cf3638cc7448936
ophélie <ophelieleleu@yahoo.fr>
2021-04-19 11:22:23 +0200

    Exo 05 jour 04

--- jour04.js
+++ jour04.js
@@ -64,11 +64,11 @@ console.log("Exo 03");
 function checkIfEven(num) {
 
     let resultat = 0;
-      
-    if (resultat = num % 2 === 0 ) {
+
+    if (resultat = num % 2 === 0) {
         console.log("even");
-    
-    } else {   
+
+    } else {
         console.log("odd");
 
     }
@@ -78,3 +78,53 @@ function checkIfEven(num) {
 console.log(checkIfEven(15))
 
 console.log();
+
+
+// 04 Compare
+
+console.log("Exo 04");
+
+
+
+function compare(num1, num2) {
+
+
+    if (num1 > num2) {
+        console.log (num1 +" is bigger");
+
+    } else if (num1 < num2) {
+
+        console.log (num2 +" is bigger");
+
+    } else {
+
+        console.log("both are the same");
+
+    }
+}
+
+        console.log (compare(15,15));
+
+
+
+    console.log();
+
+
+
+    // 05 - add Up
+
+    console.log("Exo 05");
+
+    var total = 0;
+    var limit = 12;
+
+    for (var i = 0; i <= limit; i++) {
+    
+    total += i;
+
+    console.log("total partiel:", total);
+    }
+
+    console.log(total);
+
+    console.log();
