b4041072eddebe0b2fcfff2e33bd0ea642539796
ophélie <ophelieleleu@yahoo.fr>
2021-04-21 12:26:49 +0200

    Exo 03 fichier

--- fichier.js
+++ fichier.js
@@ -11,3 +11,80 @@ function sortLetters (phrase) {
     return tri;
 }
 console.log(sortLetters("konexio"));
+
+console.log();
+
+// 02 - XOXO
+
+console.log("Exo 02");
+
+function countEach(mot) {
+    console.log("La valeur de mon parametre est " + mot);
+    let countO = 0;
+    let countX = 0;
+  
+    let decoupage = mot.split("");
+  
+    for (let i = 0; i < decoupage.length; i++) {
+      if (decoupage[i] === "x") {
+        countX += 1;
+      } else {
+        countO += 1;
+        // count0++; est équivalent;
+      }
+    }
+  
+    if (countX === countO) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  console.log(countEach("xoxoxoxo"));
+  
+  let retour = countEach("xoxoxoxoooo");
+  console.log(retour);
+  let retour2 = countEach("xoxoxoxoxxx");
+  console.log(retour2);
+  retour2 = countEach("xoxoxoxoxxxxxoxoxoox");
+  console.log(retour2);
+
+
+
+// 03 - Palindrome
+
+console.log("Exo 03 pas la bonne facon");
+
+
+function checkPal(palindrome) {
+
+   let cut = palindrome.split("").reverse();
+   
+   let colle = cut.join();
+
+    return colle;
+}
+
+console.log(checkPal("racecar"))
+console.log(checkPal("laptop"))
+
+
+
+console.log("Exo 03-02");
+
+function checkPalDeux(palindromeDeux) {
+
+let result = '';
+
+for (var i = palindromeDeux.length - 1; i !== -1; i--) {
+    result += palindromeDeux.charAt(i)
+}
+return result;
+}
+
+console.log(checkPalDeux("racecar"))
+console.log(checkPalDeux("laptop"))
+
+
+
