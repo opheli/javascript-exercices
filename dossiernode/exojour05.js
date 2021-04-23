59a32c117b0de2605db5167a5ab4f66b66ca07d8
ophélie <ophelieleleu@yahoo.fr>
2021-04-23 10:14:57 +0200

    Exo 2 node jour 05 a revoir

--- dossiernode/exojour05.js
+++ dossiernode/exojour05.js
@@ -1,7 +1,7 @@
 
 //01 CALCULATOR //
 
-let arguments = process.argv.slice(2);
+/*let arguments = process.argv.slice(2);
 console.table(arguments);
 
 function calculate(num1, num2, operateur) {
@@ -19,9 +19,47 @@ function calculate(num1, num2, operateur) {
         total = num1*num2;
     }
     return total;
+
 }
+
 let chiffre1 = parseInt(arguments[0])
 let chiffre2 = parseInt(arguments[1])
 let operateur = arguments[2]
 let resultat = calculate(chiffre1, chiffre2, operateur);
-console.log(resultat)
+console.log(resultat)*/
+
+
+// 02 Table
+
+let arguments = process.argv.slice(2);
+
+
+function multiply(numEntier) {
+    let limit = 10;
+    let total = 0;
+    for (var i = 1; i <= limit; i++) {
+        total = i*numEntier;
+        console.log(total + " = " + numEntier + " x " + i);
+    }
+    
+    return total
+   
+}
+
+
+/*if (process.argv.length === 3) {
+
+    let numEntier = parseInt(arguments[0]);
+    let total = multiply (numEntier);
+
+} else  {
+
+    console.log("error");
+
+
+}*/
+
+//console.log(multiply)
+
+let numEntier = parseInt(arguments[0]);
+let total = multiply (numEntier);
