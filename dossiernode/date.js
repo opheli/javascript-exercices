72fd200a9fb267cd5e66954fea28dc3857e48696
ophélie <ophelieleleu@yahoo.fr>
2021-04-27 15:45:05 +0200

    Exo date 02 jour06 avec internet

--- dossiernode/date.js
+++ dossiernode/date.js
@@ -9,4 +9,20 @@ function formatDate(date) {
 
 }
 formatDate('1995-01-22');
+//console.log(nowTest.toLocaleString());
+
+
+// 02 Age
+
+
+console.log('exo02');
+
+
+function calculateAge(birthday)
+{
+  birthday = new Date(birthday);
+  return new Number((new Date().getTime() - birthday.getTime()) / 31536000000).toFixed(0);
+}
+
+console.log(calculateAge("3 January 1969"));
 
