1b1efb246a9182571315d90330f07bc6f7cee182
ophélie <ophelieleleu@yahoo.fr>
2021-04-27 16:52:55 +0200

    Exo validator jour06 a finir

--- dossiernode/date.js
+++ dossiernode/date.js
@@ -12,7 +12,10 @@ formatDate('1995-01-22');
 //console.log(nowTest.toLocaleString());
 
 
+
 // 02 Age
+
+// avant que tu demandes c'est internet et elle fonctionne avant 1970
 
 
 console.log('exo02');
@@ -26,3 +29,31 @@ function calculateAge(birthday)
 
 console.log(calculateAge("3 January 1969"));
 
+
+
+// 03 Validator
+
+
+console.log('exo03');
+
+var prompt = require("prompt");
+
+prompt.start();  // démarre le prompt
+
+function onErr(err) { // permet de gérer les erreurs
+  console.log(err);
+  return;
+}
+
+prompt.get(["username", "email"],
+
+function  checkProfile(err, res) {  // demande à obtenir les propriétés "username" et "email"
+    
+  if ("username" !== /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/igm) {
+		return onErr(error);
+	}
+
+  console.log("Données reçues :");
+  console.log("=> Username : " + res.username); // affiche le résultat pour la propriété "username"
+  console.log("=> Email : " + res.email); // affiche le résultat pour la propriété "email"
+});
