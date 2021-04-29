54c908ea719547def60108c41cc7cec9e4031ba8
ophélie <ophelieleleu@yahoo.fr>
2021-04-29 11:47:29 +0200

    Exo 03 jour 07

--- dossiernode/mapexo.js
+++ dossiernode/mapexo.js
@@ -9,3 +9,36 @@ let doubles = numbers.map(function(num) {
 });
 
 console.log(doubles);
+
+
+// 03 - Map Names
+
+console.log("Exo 03");
+
+
+
+let longNames = [
+    {
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith"
+	}
+];
+
+let shortNames  = longNames.map(
+    function(element) {
+        let total = element.firstName +" "+ element.lastName;
+    return "name:" +" "+ total; 
+   
+});
+console.log(shortNames);
+
+
+//04 - Filter Numbers
+
+console.log("Exo 04");
+
+let array = [1, "toto", 34, "javascript", 8];
